024c6553ed51ca5f861396b0b8e30a336f7a3fd8
{Abhishek8211} <abhishekkrbgp8211@gmail.com>
2026-01-20 14:26:24 +0530

    Add test component

---
+++ src/TestApp.jsx
@@ -0,0 +1,20 @@
+function TestApp() {
+  return (
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        background: "red",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        color: "white",
+        fontSize: "48px",
+      }}
+    >
+      TEST - If you see this, React is working!
+    </div>
+  );
+}
+
+export default TestApp;
